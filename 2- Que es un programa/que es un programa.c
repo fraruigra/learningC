f267aa8d3f53dd5acdc293a3d3c43e60294e0fad
fraruigra <fraruigra@outlook.com>
2023-01-17 18:43:28 +0100

    17/1

---
+++ 2- Que es un programa/que es un programa.c
@@ -0,0 +1,10 @@
+El programa tiene un objetivo bien definido
+
+La actividad del programador es resolver problemas, teniendo el ordenador como herramienta
+fundamental
+
+Para la resolución de problemas tenemos que utilizar algoritmos
+
+Debemos utilizar diagramas de flujos para solucionar algoritmos. Es más sencillo hacerlo
+gráficamente antes que el modo texto
+
